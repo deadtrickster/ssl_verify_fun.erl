499f40b8ce353a38c5594df3b257fe83f34cd98e
Ilya Khaprov <dead.trickster@gmail.com>
2015-04-02 07:49:41 -0700

    add hex.pm package.exs

---
+++ package.exs
@@ -0,0 +1,30 @@
+defmodule Hackney.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :ssl_verify_hostname,
+     version: "1.0.4",
+     description: description,
+     package: package,
+     fetch: fetch]
+  end
+
+  defp description do
+    """
+    Hostname verification library for Erlang
+    """
+  end
+
+  defp package do
+    [contributors: ["Ilya Khaprov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/deadtrickster/ssl_verify_hostname.erl"},
+     files: ["src", "README.md", "LICENSE", "Makefile"]]
+  end
+
+  defp fetch do
+    [scm: :git,
+     url: "git://github.com/deadtrickster/ssl_verify_hostname.erl.git",
+     tag: "1.0.4"]
+  end
+end
