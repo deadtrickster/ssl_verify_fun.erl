1105981e2c3afb40dcda46163aa54c3f931a1e0b
Ilya Khaprov <dead.trickster@gmail.com>
2015-05-31 11:40:02 -0700

    bump version

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ defmodule Hackney.Mixfile do
 
   def project do
     [app: :ssl_verify_hostname,
-     version: "1.0.4",
+     version: "1.0.5",
      description: description,
      package: package,
      fetch: fetch]
@@ -25,6 +25,6 @@ defmodule Hackney.Mixfile do
   defp fetch do
     [scm: :git,
      url: "git://github.com/deadtrickster/ssl_verify_hostname.erl.git",
-     tag: "1.0.4"]
+     tag: "1.0.5"]
   end
 end
