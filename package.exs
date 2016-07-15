33406f6decdcb9f03cf1e69e34728a288af156a0
Ilya Khaprov <dead.trickster@gmail.com>
2016-07-15 13:43:31 +0300

    bump to v1.1.1

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ defmodule SSLVerifyFun.Mixfile do
 
   def project do
     [app: :ssl_verify_fun,
-     version: "1.1.0",
+     version: "1.1.1",
      description: description,
      package: package]
   end
