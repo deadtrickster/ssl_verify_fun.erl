c5718226b0b9f3d1a38ef6ca3c3b4c75f53dda92
Ilya Khaprov <i.khaprov@gmail.com>
2018-07-15 11:29:36 +0300

    bump to 1.1.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SSLVerifyFun.Mixfile do
 
   def project do
     [app: :ssl_verify_fun,
-     version: "1.1.3",
+     version: "1.1.4",
      description: description(),
      package: package()]
   end
